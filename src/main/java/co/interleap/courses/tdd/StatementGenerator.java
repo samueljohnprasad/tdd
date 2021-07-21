04198cf82464b8c975848862885c5fa775288f7f
samueljohnprasad <samgolla.johnprasad777@gmail.com>
2021-07-21 14:34:31 +0530

    refactored

--- src/main/java/co/interleap/courses/tdd/StatementGenerator.java
+++ src/main/java/co/interleap/courses/tdd/StatementGenerator.java
@@ -4,19 +4,21 @@ public class StatementGenerator {
 	
 	private int distance;
 	private int time;
+	private static final int FARE_PER_KM=10;
+	private static final int FARE_PER_MINUTE=1;
 	StatementGenerator(int distance, int time){
 		this.time=time;
 		this.distance=distance;
 		
 	}
 	
-	public int statementGenerator() {
+	public int create() {
 		
 		 if(distance< -1 || time< -1) {
 			 return 0;
 		 }
 		
-		return ((distance*10)+(time*1));
+		return ((distance*FARE_PER_KM)+(time*FARE_PER_MINUTE));
 	}
 
 }
--- src/test/java/co/interleap/courses/tdd/StatementGeneratorTest.java
+++ src/test/java/co/interleap/courses/tdd/StatementGeneratorTest.java
@@ -7,27 +7,27 @@ import org.junit.Test;
 public class StatementGeneratorTest {
 
 	@Test
-	public void test() {
+	public void bothDistanceAndtimePositiveValues() {
 	   int expected = 120;
-	   int actual = new StatementGenerator(10,20).statementGenerator();
+	   int actual = new StatementGenerator(0,20).create();
 	}
 	
 	@Test
 	public void distancePostiveAndTimeNegativeValues() {
 	   int expected = 0;
-	   int actual = new StatementGenerator(10,20).statementGenerator();
+	   int actual = new StatementGenerator(10,20).create();
 	}
 	
 	@Test
 	public void distanceNegativeAndTimePostiveValues() {
 	   int expected = 0;
-	   int actual = new StatementGenerator(10,20).statementGenerator();
+	   int actual = new StatementGenerator(10,20).create();
 	}
 
 	@Test
 	public void bothDistanceAndtimeNegativeValues() {
 	   int expected = 0;
-	   int actual = new StatementGenerator(10,20).statementGenerator();
+	   int actual = new StatementGenerator(10,20).create();
 	}
 
 }
