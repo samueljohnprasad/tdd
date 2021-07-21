7ae0adf603c0d8b629a29a31b38b7eeb9cf76a49
samueljohnprasad <samgolla.johnprasad777@gmail.com>
2021-07-21 14:10:26 +0530

    refactored

--- src/main/java/co/interleap/courses/tdd/VowelCounter.java
+++ src/main/java/co/interleap/courses/tdd/VowelCounter.java
@@ -1,26 +1,10 @@
 package co.interleap.courses.tdd;
 
-import java.util.HashMap;
-import java.util.Map;
 
 public class VowelCounter {
 	
 	public int vowelCounter(String string) {
-		string= string.toLowerCase();
-		 Map <Character,Integer> map = new HashMap<>();
-		 map.put('a',0);
-		 map.put('e',0);
-		 map.put('i',0);
-		 map.put('o',0);
-		 map.put('u',0);
-		 
-		 int count=0;
-		 for(char c: string.toCharArray()) {
-			  if( map.containsKey(c)) {
-				  ++count;
-			  }
-		 }
-		 
-		return count;
+		final String res= "(?i)[^aeioius]+";
+		return string.replaceAll(res,"").length();
 	}
 }
