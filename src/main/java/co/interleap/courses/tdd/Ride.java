ed88f41e37c264516f43e07b8013f6fcf355f7b3
samueljohnprasad <samgolla.johnprasad777@gmail.com>
2021-07-21 17:12:24 +0530

    refactored and moved fare to the ride class

--- src/main/java/co/interleap/courses/tdd/Ride.java
+++ src/main/java/co/interleap/courses/tdd/Ride.java
@@ -4,18 +4,21 @@ public class Ride {
 
 	   private int minutes;
 	   private int distance;
+	   private static final int FARE_PER_KM=10;
+		private static final int FARE_PER_MINUTE=1;
 	   
 	   Ride(int distance, int minutes){
 		   this.minutes=minutes;
 		   this.distance=distance;
 	   }
 	   
-	   public int getMinutes() {
-		   return this.minutes;
-	   }
-	   
-	   public int getDistance() {
-		   return this.distance;
-	   }
+	 
+	   public int getTotalFare() {
+		      if(distance>=0 && minutes>=0 ) {		       
+			   return((distance*FARE_PER_KM)+(minutes*FARE_PER_MINUTE));
+		      }
+		      
+		      return 0;
+		}
 }
 
--- src/main/java/co/interleap/courses/tdd/StatementGenerator.java
+++ src/main/java/co/interleap/courses/tdd/StatementGenerator.java
@@ -5,28 +5,19 @@ import java.util.List;
 public class StatementGenerator {
 	
 	
-	private static final int FARE_PER_KM=10;
-	private static final int FARE_PER_MINUTE=1;
+	
 	
 	public int create(List<Ride> list) {
 		int count =0;
 		for(Ride ride: list) {
 		
-	    	if(ride.getMinutes()<0 ||ride.getDistance() <0) {
-				  continue;
-			}
-			
-			count+=fare(ride);
-			
+	    	 count+=ride.getTotalFare();			
 		}
 		
 		
-		//return 
 		return count;
 	}
 	
-	private int fare(Ride ride) {
-		return ((ride.getDistance()*FARE_PER_KM)+(ride.getMinutes()*FARE_PER_MINUTE));
-	}
+	
 
 }
