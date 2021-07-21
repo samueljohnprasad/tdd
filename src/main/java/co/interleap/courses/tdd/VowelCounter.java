945c2264879acb41746bbde6327a1586191c5d14
samueljohnprasad <samgolla.johnprasad777@gmail.com>
2021-07-21 13:56:12 +0530

    CapsVowelandConsonants

--- src/main/java/co/interleap/courses/tdd/VowelCounter.java
+++ src/main/java/co/interleap/courses/tdd/VowelCounter.java
@@ -1,8 +1,26 @@
 package co.interleap.courses.tdd;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class VowelCounter {
 	
 	public int vowelCounter(String string) {
-		return string.length();
+		string= string.toLowerCase();
+		 Map <Character,Integer> map = new HashMap<>();
+		 map.put('a',0);
+		 map.put('e',0);
+		 map.put('i',0);
+		 map.put('o',0);
+		 map.put('u',0);
+		 
+		 int count=0;
+		 for(char c: string.toCharArray()) {
+			  if( map.containsKey(c)) {
+				  ++count;
+			  }
+		 }
+		 
+		return count;
 	}
 }
--- src/test/java/co/interleap/courses/tdd/VowelCounterTest.java
+++ src/test/java/co/interleap/courses/tdd/VowelCounterTest.java
@@ -4,31 +4,42 @@ import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
-public class VowelCounterTest {
-
-    @Test
+public class VowelCounterTest {   		
+	@Test
     public void shoudCountZeroWithEmptyString(){
     	int expected =0;
     	int actual=new VowelCounter().vowelCounter("");
     	assertEquals(expected,actual);
-    	
-    	
-    	
     }
-    
-    @Test
+	
+	@Test
     public void singleVowelAndNoConsonats() {
     	int expected =1;
     	int actual=new VowelCounter().vowelCounter("a");
     	assertEquals(expected,actual);
-    	
     }
-    
-    @Test
-    public void MultipleVowelsAndNoConsonats() {
+   
+	@Test
+    public void multipleVowelsAndNoConsonats() {
     	int expected =5;
     	int actual=new VowelCounter().vowelCounter("aieuo");
     	assertEquals(expected,actual);
-    	
     }
+
+	@Test
+    public void vowelsAndconsonants() {
+    	int expected =5;
+    	int actual=new VowelCounter().vowelCounter("aieuowdf");
+    	assertEquals(expected,actual);
+    }
+	
+	@Test
+	public void capsVowelsAndconsonants() {
+    	int expected =5;
+    	int actual=new VowelCounter().vowelCounter("AIeuoWdf");
+    	assertEquals(expected,actual);
+    }
+	
+	
+    
 }
